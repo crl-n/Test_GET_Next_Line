9e31445cfe8d948f13a7db579190c1345c4b9c1f
Carl Nysten <nysten.carl@gmail.com>
2021-12-05 23:22:59 +0200

    main.c basic test with a few lines of text

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int	main(void)
 {
+	char	*line;
+	int		ret;
+	int		fd;
+
 	printf("Test GET_Next_Line\n");
+
+	fd = open("hello.txt", O_RDONLY);
+	if (fd > 2)
+	{
+		while (1)
+		{
+			ret = get_next_line(fd, &line);
+			if (ret < 1)
+				break ;
+			printf("line: %s\n", line);
+			printf("ret: %d\n", ret);
+			free(line);
+		}
+		close(fd);
+	}
 	return (0);
 }
