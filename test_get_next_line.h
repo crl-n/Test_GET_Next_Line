c964a0f26b0ee601f700397b54d0618d0d3a2ea2
cnysten <nysten.carl@gmail.com>
2021-12-08 17:01:07 +0200

    test_get_next_line.h created, kprint color defines added

---
+++ test_get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_get_next_line.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 20:28:28 by cnysten           #+#    #+#             */
+/*   Updated: 2021/12/08 17:00:45 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_GET_NEXT_LINE_H
+# define TEST_GET_NEXT_LINE_H
+
+# define RED   "\x1B[31m"
+# define GRN   "\x1B[32m"
+# define YEL   "\x1B[33m"
+# define BLU   "\x1B[34m"
+# define MAG   "\x1B[35m"
+# define CYN   "\x1B[36m"
+# define WHT   "\x1B[37m"
+# define RESET "\x1B[0m"
+
+#endif
