c5b365a49fde2cfdbb204672ccf24d90588b8bcf
cnysten <nysten.carl@gmail.com>
2021-12-10 11:23:39 +0200

    run_tests.sh colors added

--- run_tests.sh
+++ run_tests.sh
@@ -4,21 +4,44 @@
 
 ################################################################################
 
+# Colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+reset=`tput sgr0`
+
 # Function for modifying BUFF_SIZE
 function set_buff_size() {
 	sed -i'' -e 's/^# define BUFF_SIZE.*/# define BUFF_SIZE '$1'/g' '../GET_Next_Line/get_next_line.h'
 	rm ../GET_Next_Line/get_next_line.h-e
 }
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# Basic tests
+echo "${magenta}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "\t\t\t\tBasic Tests"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 set_buff_size 8
+echo "# BUFF SIZE: 8${reset}"
 
 # Line of 8 chars
+echo "${green}\n# One line of 8 chars${reset}"
 echo "abcdabcd" > input
 ./test_gnl
+
+# Two lines of 8 chars
+echo "${green}\n# Two lines of 8 chars${reset}"
+echo "abcdabcd" > input
+echo "abcdabcd" > input
+./test_gnl
+
+# Middle tests
+echo "${magenta}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\t\t\t\tMiddle Tests"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${reset}"
 
 # Too big BUFF_SIZE
 for i in {1..8}
